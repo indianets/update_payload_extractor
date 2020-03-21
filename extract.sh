d5d67526bdf545fbace0b4c14991a0fbc2f7be03
Vijay <vijay@indianets.com>
2020-03-21 19:30:23 +0530

    Shell Wrapper to take zipfile as argument

--- extract.sh
+++ extract.sh
@@ -1,20 +1,32 @@
-#!/bin/sh
+#!/usr/bin/env bash
 #
 # wrapper shell script
 # copyright: @indianets
 #
 
-[[ -z $1 ]] || { echo "Usage: $0 partitions OR $0 --list"; exit 1; }
+[[ -z $2 ]] && { echo "Usage: $(basename $0) zipfile partitions OR $(basename $0) zipfile --list"; exit 1; }
 
-dir="$PWD"
-if [[ $1 = --list ]]; then
-  $dir/extract.py --list_partitions
+workdir="$PWD"
+tooldir="$(dirname $0)"
+
+[[ -f $workdir/$1 ]] || { echo "$1 is not a file"; exit 1; }
+
+echo Extracting payload.bin ..
+unzip $workdir/$1 payload.bin
+[[ -f $workdir/payload.bin ]] || { echo "Failed extracting payload.bin from zip"; exit 1; }
+
+if [[ $2 = "--list" ]]; then
+  $tooldir/extract.py $workdir/payload.bin --list_partitions
 else
-  $dir/extract.py payload.bin --output_dir $dir --partitions $@
+  shift
+  $tooldir/extract.py $workdir/payload.bin --output_dir $workdir --partitions $@
   for part in $@; do
     if [[ -e $part ]]; then
-      \mv $part $part.img
+      mv $part $part.img
       ls -l $part.img
     fi
   done
 fi
+
+echo Deleting payload.bin ..
+rm $workdir/payload.bin
